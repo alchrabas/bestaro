dce44194d576889ce3bd060a9fc7a21216366da4
Aleksander <grkalk@gmail.com>
2017-07-11 22:18:49 +0200

    Refactor facebook collector

--- src/main/scala/bestaro/collectors/FacebookCollector.scala
+++ src/main/scala/bestaro/collectors/FacebookCollector.scala
@@ -1,30 +1,41 @@
 package bestaro.collectors
 
-import java.nio.file.{Files, Paths, StandardCopyOption}
+import java.nio.file.Path
+import java.util.UUID
 
-import facebook4j.FacebookFactory
+import bestaro.core.ProgressStatus.LOST
+import bestaro.core.RawRecord
+import bestaro.util.ImageUtil
+import facebook4j.{FacebookFactory, Post}
 
-import scala.util.Random
+class FacebookCollector(recordConsumer: RawRecord => Unit) {
 
-class FacebookCollector {
-
-  val random = new Random()
-
-  def collect(): Unit = {
+  def collect(recordConsumer: RawRecord => Unit): Unit = {
     val facebook = new FacebookFactory().getInstance
 
     val foundGroups = facebook.search().searchGroups("ZaginioneKrakow")
     val foundGroup = foundGroups.get(0)
     val result = facebook.groups().getGroupFeed(String.valueOf(foundGroup.getId))
     println(s"Found ${result.size()} posts")
-    for (i <- 0 to 3) {
-      val a = result.get(i)
-      println(s"[link: ${a.getLink}, ${a.getUpdatedTime}] ${a.getMessage}")
-      if (a.getPicture != null) {
-        Files.copy(a.getPicture.openStream(),
-          Paths.get("pictures", random.nextString(10)),
-          StandardCopyOption.REPLACE_EXISTING)
-      }
+
+    val indexes = 0 until result.size()
+
+    indexes
+      .iterator
+      .map(result.get)
+      .map(postToRecord)
+      .foreach(recordConsumer)
+  }
+
+  def postToRecord(post: Post): RawRecord = {
+    println(s"[link: ${post.getLink}, ${post.getUpdatedTime}] ${post.getMessage}")
+    val uuid = UUID.randomUUID()
+    var picturePath: Option[Path] = Option.empty
+    if (post.getPicture != null) {
+      picturePath = Some(ImageUtil.saveImage(uuid, post.getPicture.openStream()))
     }
+
+    RawRecord(uuid.toString, LOST, post.getMessage, post.getUpdatedTime.getTime,
+      picturePath.map(_.toString).toList, Option(post.getLink).map(_.toString).orNull)
   }
 }
