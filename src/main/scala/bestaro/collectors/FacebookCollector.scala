02c157590475de54073dc87cc352626af241fe9b
Aleksander <grkalk@gmail.com>
2017-07-22 22:55:22 +0200

    Support pagination for Facebook Graph API

--- src/main/scala/bestaro/collectors/FacebookCollector.scala
+++ src/main/scala/bestaro/collectors/FacebookCollector.scala
@@ -6,7 +6,8 @@ import java.util.UUID
 import bestaro.core.ProgressStatus.LOST
 import bestaro.core.RawRecord
 import bestaro.util.ImageUtil
-import facebook4j.{FacebookFactory, Post}
+import facebook4j._
+import scala.collection.JavaConverters._
 
 class FacebookCollector(recordConsumer: RawRecord => Unit) {
 
@@ -15,27 +16,44 @@ class FacebookCollector(recordConsumer: RawRecord => Unit) {
 
     val foundGroups = facebook.search().searchGroups("ZaginioneKrakow")
     val foundGroup = foundGroups.get(0)
-    val result = facebook.groups().getGroupFeed(String.valueOf(foundGroup.getId))
+    val eater = new FacebookEater(facebook, foundGroup.getId)
+
+    val result = eater.fetch()
     println(s"Found ${result.size()} posts")
 
-    val indexes = 0 until result.size()
-
-    indexes
+    result
       .iterator
-      .map(result.get)
+      .asScala
       .map(postToRecord)
       .foreach(recordConsumer)
   }
 
+  class FacebookEater(facebook: Facebook, groupId: String) {
+    private var lastPage: Option[Paging[Post]] = None
+    private val READING = new Reading().limit(25)
+      .fields("message", "link", "id", "permalink_url", "created_time", "attachments")
+
+    def fetch(): ResponseList[Post] = {
+      val responseList = if (lastPage.isEmpty) {
+        facebook.groups().getGroupFeed(groupId, READING)
+      } else {
+        facebook.fetchNext(lastPage.get)
+      }
+      lastPage = Some(responseList.getPaging)
+
+      responseList
+    }
+  }
+
   def postToRecord(post: Post): RawRecord = {
-    println(s"[link: ${post.getLink}, ${post.getUpdatedTime}] ${post.getMessage}")
+    println(s"[link: ${post.getPermalinkUrl}, ${post.getCreatedTime}] ${post.getMessage}")
     val uuid = UUID.randomUUID()
     var picturePath: Option[Path] = Option.empty
     if (post.getPicture != null) {
       picturePath = Some(ImageUtil.saveImage(uuid, post.getPicture.openStream()))
     }
 
-    RawRecord(uuid.toString, LOST, post.getMessage, post.getUpdatedTime.getTime,
-      picturePath.map(_.toString).toList, Option(post.getLink).map(_.toString).orNull)
+    RawRecord(uuid.toString, LOST, post.getMessage, post.getCreatedTime.getTime,
+      picturePath.map(_.toString).toList, Option(post.getPermalinkUrl).map(_.toString).orNull)
   }
 }
