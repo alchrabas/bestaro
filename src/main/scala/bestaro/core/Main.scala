76e38842260d6ba7924640d53eab3094fbbbb7a5
Aleksander <grkalk@gmail.com>
2017-07-23 23:36:39 +0200

    Extract (de)serialization of records to a separate class

--- src/main/scala/bestaro/core/Main.scala
+++ src/main/scala/bestaro/core/Main.scala
@@ -7,7 +7,7 @@ import java.nio.file.{Files, Paths}
 import bestaro.collectors.util.SlowHttpDownloader
 import bestaro.collectors.{FacebookCollector, OlxCollector}
 import bestaro.core.processors.PlaintextProcessor
-import upickle.default._
+
 
 object Main {
 
@@ -19,59 +19,21 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val printResult = (record: RawRecord) => println(record)
+    val jsonSerializer = new JsonSerializer
 
     OPTION match {
       case FB =>
-        val fb = new FacebookCollector(saveInJson, recordAlreadyExists)
-        fb.collect(saveInJson)
+        val fb = new FacebookCollector(jsonSerializer.saveInJson, jsonSerializer.recordAlreadyExists)
+        fb.collect(jsonSerializer.saveInJson)
       case OLX =>
         val olx = new OlxCollector(new SlowHttpDownloader)
-        olx.collect(saveInJson)
+        olx.collect(jsonSerializer.saveInJson)
       case PROCESS =>
-        val records = readRecordsFromFile
+        val records = jsonSerializer.readRecordsFromFile
         val processor = new PlaintextProcessor
         for (record <- records) {
           processor.process(record)
         }
     }
-  }
-
-  private def recordAlreadyExists(record: RawRecord): Boolean = {
-    readRecordsFromFile.exists(_.recordId == record.recordId)
-  }
-
-  import RecordId._
-  private def saveInJson(record: RawRecord): Unit = {
-    var listOfRecords: Seq[RawRecord] = readRecordsFromFile
-    if (!listOfRecords.exists(_.recordId == record.recordId)) {
-      listOfRecords = listOfRecords :+ record
-    }
-    saveFile(write(listOfRecords))
-  }
-
-  private def readRecordsFromFile: Seq[RawRecord] = {
-    val fileContents = readFile()
-    read[Seq[RawRecord]](fileContents)
-      .filter {
-        record => record.link != null && record.link.contains("facebook")
-      }
-  }
-
-  private def readFile(): String = {
-    if (!Paths.get("rawData.json").toFile.exists()) {
-      new File("rawData.json").createNewFile()
-    }
-    val encoded = Files.readAllBytes(Paths.get("rawData.json"))
-    if (encoded.isEmpty) {
-      new String("[]")
-    } else {
-      new String(encoded, StandardCharsets.UTF_8)
-    }
-  }
-
-  private def saveFile(json: String): Unit = {
-    val jsonWriter = new FileWriter("rawData.json")
-    jsonWriter.write(json)
-    jsonWriter.close()
   }
 }
