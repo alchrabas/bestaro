4210735be7fd8a3bb185d589a4f2e31a330b7e57
Aleksander <grkalk@gmail.com>
2017-07-25 17:37:51 +0200

    Manager for saving/loading list of tagged records to CSV

---
+++ src/main/scala/bestaro/helpers/TaggedRecordsManager.scala
@@ -0,0 +1,59 @@
+package bestaro.helpers
+
+import java.io.File
+
+import bestaro.core.{JsonSerializer, RecordId}
+import com.github.tototoshi.csv.{CSVReader, CSVWriter}
+import upickle.default.{read, write}
+
+import scala.collection.mutable.ListBuffer
+
+object TaggedRecordsManager {
+
+  def main(args: Array[String]): Unit = {
+    readTaggedRecordsFromCsv()
+  }
+
+  def readTaggedRecordsFromCsv(): List[TaggedRecord] = {
+    val reader = CSVReader.open("records-for-tagging.csv")
+    val listOfRows = reader.allWithHeaders()
+
+    val taggedRecords = ListBuffer[TaggedRecord]()
+    for ((row, index) <- listOfRows.view.zipWithIndex if index < 120) {
+      val recordId = read[RecordId](row("ID"))
+      val locs =
+        stringToList(row("Location-1")) ::: stringToList(row("Location-2")) ::: stringToList(row("Location-3"))
+      val altLocs = stringToList(row("Location-1-opt"))
+      val isKrakow = Option(row("Krakow")).contains("1")
+      val date = row("Date")
+      val animalType = row("Type")
+      val status = row("Status")
+
+      if (isKrakow) {
+        taggedRecords.append(TaggedRecord(recordId, locs, altLocs, animalType, status))
+      }
+    }
+
+    taggedRecords.toList
+  }
+
+  case class TaggedRecord(recordId: RecordId, locs: List[String], altLocs: List[String],
+                          animalType: String, status: String)
+
+  private def stringToList(str: String): List[String] = {
+    Option(str).filter(_.nonEmpty).toList
+  }
+
+  private def saveInCsv(): Unit = {
+    import RecordId._
+    if (new File("records-for-tagging.csv").exists()) {
+      throw new Exception("Do not overwrite this csv file")
+    }
+    val writer = CSVWriter.open("records-for-tagging.csv")
+    val jsonSerializer = new JsonSerializer
+    for (record <- jsonSerializer.readRecordsFromFile) {
+      writer.writeRow(List(write(record.recordId), record.message))
+    }
+    writer.close()
+  }
+}
