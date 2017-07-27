449cd53bbd784cc27d1ed0df58be85899a22eaa7
Aleksander <grkalk@gmail.com>
2017-07-27 23:58:02 +0200

    Extract File IO operations to the util class

--- src/main/scala/bestaro/core/JsonSerializer.scala
+++ src/main/scala/bestaro/core/JsonSerializer.scala
@@ -1,47 +1,26 @@
 package bestaro.core
 
-import java.io.{File, FileWriter}
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
+import bestaro.util.FileIO
 import upickle.default.{read, write}
 
 class JsonSerializer {
 
   def readRecordsFromFile: Seq[RawRecord] = {
-    val fileContents = readFile()
+    val fileContents = FileIO.readFile("rawData.json", "[]")
     read[Seq[RawRecord]](fileContents)
       .filter {
         record => record.link != null && record.link.contains("facebook")
       }
   }
 
-  import RecordId._
-
   def saveInJson(record: RawRecord): Unit = {
     var listOfRecords: Seq[RawRecord] = readRecordsFromFile
     if (!listOfRecords.exists(_.recordId == record.recordId)) {
       listOfRecords = listOfRecords :+ record
     }
-    saveFile(write(listOfRecords))
+    FileIO.saveFile("rawData.json", write(listOfRecords))
   }
 
-  private def readFile(): String = {
-    if (!Paths.get("rawData.json").toFile.exists()) {
-      new File("rawData.json").createNewFile()
-    }
-    val encoded = Files.readAllBytes(Paths.get("rawData.json"))
-    if (encoded.isEmpty) {
-      new String("[]")
-    } else {
-      new String(encoded, StandardCharsets.UTF_8)
-    }
-  }
-
-  private def saveFile(json: String): Unit = {
-    val jsonWriter = new FileWriter("rawData.json")
-    jsonWriter.write(json)
-    jsonWriter.close()
-  }
 
   def recordAlreadyExists(record: RawRecord): Boolean = {
     readRecordsFromFile.exists(_.recordId == record.recordId)
---
+++ src/main/scala/bestaro/util/FileIO.scala
@@ -0,0 +1,26 @@
+package bestaro.util
+
+import java.io.{File, FileWriter}
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+object FileIO {
+
+  def readFile(inputFile: String, whenEmpty: String): String = {
+    if (!Paths.get(inputFile).toFile.exists()) {
+      new File(inputFile).createNewFile()
+    }
+    val encoded = Files.readAllBytes(Paths.get(inputFile))
+    if (encoded.isEmpty) {
+      new String(whenEmpty)
+    } else {
+      new String(encoded, StandardCharsets.UTF_8)
+    }
+  }
+
+  def saveFile(outputFile: String, json: String): Unit = {
+    val jsonWriter = new FileWriter(outputFile)
+    jsonWriter.write(json)
+    jsonWriter.close()
+  }
+}
