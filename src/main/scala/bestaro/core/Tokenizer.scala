fd804fa071d3f3ddef39db9fea32c77abc5ce7b8
Aleksander <grkalk@gmail.com>
2017-08-13 17:55:33 +0200

    Split words by "/" even if no white space between them

--- src/main/scala/bestaro/core/Tokenizer.scala
+++ src/main/scala/bestaro/core/Tokenizer.scala
@@ -7,7 +7,7 @@ class Tokenizer {
       // remove everything except letters, numbers, dots, commas and white spaces
       .map(stripLinks)
       .map(_.replaceAll("[^.,/!0-9a-ząćęłńóśżźA-ZĄĆĘŁŃÓŚŻŹ \\n]", " "))
-      .map(_.replaceAll("([.,!\\n])", "$1 "))
+      .map(_.replaceAll("([.,!/\\n])", "$1 "))
       .map(_.replaceAll("[^\\S\\n]+", " "))
       .map(_.split(" ").toList)
       .getOrElse(List())
