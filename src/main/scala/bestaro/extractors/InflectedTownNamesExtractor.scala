91b5a776937be023d038922c313fe65c94b071a4
Aleksander <grkalk@gmail.com>
2017-08-24 22:25:29 +0200

    Use java HashMap instead of scala immutable Map to improve performance

--- src/main/scala/bestaro/extractors/InflectedTownNamesExtractor.scala
+++ src/main/scala/bestaro/extractors/InflectedTownNamesExtractor.scala
@@ -1,24 +1,40 @@
 package bestaro.extractors
 
+import java.util
+
 import bestaro.core.processors.Token
-import bestaro.service.{PolishTownNamesInflector, InflectedLocation, Voivodeship}
+import bestaro.service.{InflectedLocation, PolishTownNamesInflector, Voivodeship}
 
 class InflectedTownNamesExtractor {
 
-  private val townNamesInflector = new PolishTownNamesInflector
-  private val inflectedForms = townNamesInflector.generateInflectedForms(
-    townNamesInflector.loadTownEntriesFromUrzedowyWykazNazwMiejscowosci())
-  private val inflectedFormsByVoivodeship = inflectedForms.groupBy(_.location.voivodeship.get)
-  private val townEntryByVoivodeshipAndFirstWord = inflectedFormsByVoivodeship
-    .mapValues(_.groupBy(townEntry => townEntry.stripped.split(" ")(0)))
+  private val townEntryByVoivodeshipAndFirstWord = setUpTownEntryByVoivodeshipAndFirstWord()
 
   case class TownEntryMatch(townEntry: InflectedLocation, initialPos: Int, wordCount: Int)
+
+  private def setUpTownEntryByVoivodeshipAndFirstWord(): util.HashMap[Voivodeship, util.HashMap[String, Seq[InflectedLocation]]] = {
+    val townNamesInflector = new PolishTownNamesInflector
+    val inflectedForms = townNamesInflector.generateInflectedForms(
+      townNamesInflector.loadTownEntriesFromUrzedowyWykazNazwMiejscowosci())
+    val inflectedFormsByVoivodeship = inflectedForms.groupBy(_.location.voivodeship.get)
+    val scalaMap = inflectedFormsByVoivodeship
+      .mapValues(_.groupBy(townEntry => townEntry.stripped.split(" ")(0)))
+      .mapValues { scalaMap =>
+        val newMap = new util.HashMap[String, Seq[InflectedLocation]]()
+        scalaMap.foreach { case (a, b) => newMap.put(a, b) }
+        newMap
+      }
+    val newMap = new util.HashMap[Voivodeship, util.HashMap[String, Seq[InflectedLocation]]]()
+    scalaMap.foreach { case (a, b) => newMap.put(a, b) }
+    newMap
+  }
 
   def findTownNames(tokens: List[Token], voivodeship: Voivodeship): Seq[TownEntryMatch] = {
     val potentialMatches = tokens
       .zipWithIndex
       .flatMap { case (token, position) =>
-        townEntryByVoivodeshipAndFirstWord(voivodeship).get(token.stripped).toSeq.flatten
+        val firstWord = token.stripped.split(" ")(0)
+        Option(townEntryByVoivodeshipAndFirstWord.get(voivodeship).get(firstWord))
+          .toSeq.flatten
           .map(TownEntryMatch(_, position, token.stripped.split(" ").length))
       }
     potentialMatches.filter {
@@ -28,9 +44,10 @@ class InflectedTownNamesExtractor {
           townEntryMatch.initialPos,
           townEntryMatch.initialPos + townNameTokens.length
         )
-        tokensToUse.zip(townNameTokens).forall {
-          case (token, townNamePart) => token.stripped == townNamePart
-        }
+        (townEntryMatch.initialPos + townNameTokens.length <= tokens.length) &&
+          tokensToUse.zip(townNameTokens).forall {
+            case (token, townNamePart) => token.stripped == townNamePart
+          }
     }
   }
 }
