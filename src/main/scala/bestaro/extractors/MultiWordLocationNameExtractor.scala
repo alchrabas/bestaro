0b99372f723289aa003fb1ed422bcdd6a1f003fb
Aleksander <grkalk@gmail.com>
2017-09-06 22:00:43 +0200

    Try to guess location type for every multiWordName

--- src/main/scala/bestaro/extractors/MultiWordLocationNameExtractor.scala
+++ src/main/scala/bestaro/extractors/MultiWordLocationNameExtractor.scala
@@ -2,7 +2,9 @@ package bestaro.extractors
 
 import bestaro.core.processors.{LocationType, Token}
 
-case class MultiWordName(tokens: List[Token], startIndex: Int, locType: Option[LocationType] = None) {
+case class MultiWordName(tokens: List[Token], startIndex: Int,
+                         nominitavizedStripped: Option[String] = None,
+                         locType: Option[LocationType] = None) {
   def pushWord(token: Token): MultiWordName = {
     copy(tokens = tokens :+ token)
   }
@@ -34,7 +36,7 @@ class MultiWordLocationNameExtractor {
                                  foundLocationNames: Seq[MatchedInflectedLocation]
                                 ): List[MultiWordName] = {
 
-    tokens.zipWithIndex
+    val bestScoredMultiWordNames = tokens.zipWithIndex
       .filter(_._1.placenessScore >= 5)
       .sortBy(_._1.placenessScore)
       .reverse.slice(0, 5).map {
@@ -55,6 +57,26 @@ class MultiWordLocationNameExtractor {
           }.map(_._2).foldLeft(firstWord)(_.pushWord(_))
         }
     }.sortBy(_.sumScore).reverse
+
+    bestScoredMultiWordNames.map(attemptToMatchTypeAndNominativizeLocationName(foundLocationNames))
+  }
+
+  private def attemptToMatchTypeAndNominativizeLocationName(
+                                                             foundLocationNames: Seq[MatchedInflectedLocation]
+                                                           ): (MultiWordName) => MultiWordName = {
+    (multiWordName: MultiWordName) => {
+      val inflectedLocNamesInMWN = foundLocationNames.filter(inflectedLoc =>
+        inflectedLoc.initialPos >= multiWordName.startIndex &&
+          inflectedLoc.initialPos + inflectedLoc.wordCount <= multiWordName.startIndex + multiWordName.wordCount)
+        .filter(_.wordCount / multiWordName.wordCount > 0.5) // the location is most of the MWN
+
+      val locationTypes = inflectedLocNamesInMWN.map(_.inflectedLocation.location.kind).toSet
+      if (locationTypes.size == 1) {
+        multiWordName.copy(locType = Some(locationTypes.head))
+      } else {
+        multiWordName.copy(locType = Some(LocationType.UNKNOWN))
+      }
+    }
   }
 
   private def listToTuple(tokens: List[Token]): (Token, Token) = {
