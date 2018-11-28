f9dc5b5df0d6e53f3831eecc0033543990e88656
alchrabas <alchrabas@exeris.org>
2018-11-28 23:22:45 +0100

    Add (most of) English translations

--- frontend/app/assets/js/components/ReadMorePage.jsx
+++ frontend/app/assets/js/components/ReadMorePage.jsx
@@ -17,13 +17,13 @@ const ReadMorePage = ({wide, goToMap}) => {
                     {Messages("read_more.header")}
                 </p>
             </div>
-            <div key="text" className="welcome-text">
-                {Messages("read_more.text")}
-            </div>
+            <div key="text" className="welcome-text"
+                dangerouslySetInnerHTML={{__html: Messages("read_more.text")}}/>
             <div className="pure-g" style={{marginTop: "50px"}}>
                 <div className="pure-u-1-1 pure-u-lg-1-3" style={{margin: "auto"}}>
-                    <button onClick={goToMap} className="pure-button pure-button-primary big-wide-button"
-                        dangerouslySetInnerHTML={{__html: Messages("read_more.back_button")}}/>
+                    <button onClick={goToMap} className="pure-button pure-button-primary big-wide-button">
+                        {Messages("read_more.back_button")}
+                    </button>
                 </div>
             </div>
         </div>
